0afd365feb55932fbb43310a8790e3827486ff8b
Shubham Nagaria <subhamnagaria@gmail.com>
2022-03-12 01:37:19 +0530

    Time: 4 ms (81.75%), Space: 6.3 MB (49.92%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,36 @@
+/**
+* Leetcode Problem - https://leetcode.com/problems/n-queens-ii/
+* Author - Shubham Nagaria
+* Date - 12th March 2022
+N-Queens II
+**/
+class Solution {
+public:
+    void CountToatlNQueens(int i, int n, int &count, vector<vector<int>> &mat, vector<int> &col, vector<int> &ld, vector<int> &rd){
+        if(i == n ){
+            count++;
+            return;
+        }
+        for(int j =0 ; j < n; j++){
+            if(col[j]==1 || ld[n+j-i]==1 || rd[i+j]==1)
+                continue;
+            mat[i][j] = 1;
+            col[j] = 1;
+            ld[n+j-i] = 1;
+            rd[i+j]= 1;
+            CountToatlNQueens(i+1,n,count,mat,col,ld,rd);
+             mat[i][j] = 0;
+            col[j] = 0;
+            ld[n+j-i] = 0;
+            rd[i+j] = 0;
+        }
+        return;
+    }
+    int totalNQueens(int n) {
+        vector<vector<int>>mat(n,vector<int>(n,0));
+        vector<int>col(n,0), ld(2*n,0),rd(2*n,0);
+        int count=0;
+        CountToatlNQueens(0,n,count,mat,col,ld,rd);
+        return count;
+    }
+};
