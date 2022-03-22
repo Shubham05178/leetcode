d01fd73924fd2c7a41deccef70e4d173f93336e1
Shubham Nagaria <subhamnagaria@gmail.com>
2022-03-22 09:07:36 +0530

    Time: 42 ms (71.93%), Space: 7.6 MB (78.14%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,33 @@
+/**
+* Leetcode Problem - https://leetcode.com/problems/wildcard-matching/
+* Author - Shubham Nagaria
+* Date - 22nd March 2022
+Wildcard Matching
+**/
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        bool dp[n+1][m+1];
+        dp[0][0] = true;
+        for(int i = 0 ; i <= n; ++i){
+            for(int j = 0; j <= m; ++j){
+                if(i == 0 && j == 0) continue;
+                if(i == 0){
+                    if(p[j-1]=='*') dp[i][j] = dp[i][j-1];
+                    else dp[i][j] = false;
+                    continue;
+                }
+                if(j == 0){
+                    dp[i][j] = false;
+                    continue;
+                }
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
+    }
+};
