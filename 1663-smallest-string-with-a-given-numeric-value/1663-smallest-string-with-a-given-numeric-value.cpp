74ec6a392700fc4134f8012727a9308e6af679bd
Shubham Nagaria <subhamnagaria@gmail.com>
2022-03-22 10:06:08 +0530

    Time: 62 ms (48.80%), Space: 27 MB (59.30%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -7,27 +7,25 @@ Smallest String With A Given Numeric Value
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        vector<int>arr(n,1);
+        string s;
+        for(int i =0; i < n ; i++)
+            s+='a';
         int sum_ = n;
         for(int j = n-1; j >=0; j--){
             int diff = k -(sum_-1);
             if(diff > 0){
                 if(diff > 26){
                     sum_ += 25;
-                    arr[j]=26;
+                    s[j]=25+'a';
                 }
                 else{
                     sum_ += diff-1;
-                    arr[j] = diff;
+                    s[j] = (diff-1)+'a';
                 }
             }
             else
                 break;
         }
-        string s;
-        for(int i : arr)
-            s+= string(1,(i-1)+'a');
-        
     return s;
     }
 };
