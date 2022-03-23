7e6d729b1e1f561f5f06b33428a45031cd5875d5
Shubham Nagaria <subhamnagaria@gmail.com>
2022-03-23 07:10:15 +0530

    Time: 1112 ms (5.21%), Space: 5.8 MB (95.31%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,20 @@
+/**
+* Leetcode Problem - https://leetcode.com/problems/broken-calculator/
+* Author - Shubham Nagaria
+* Date - 23rd March 2022
+Broken Calculator
+**/
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+       int c = 0;
+        while(target != startValue){
+            if(target > startValue && target%2 == 0)
+                target /= 2;
+            else
+                target +=1;
+            c++;
+        }
+        return c;
+    }
+};
