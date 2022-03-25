dfcc972d99295780eea224d81cc06d339a7af4a0
Shubham Nagaria <subhamnagaria@gmail.com>
2022-03-25 16:13:54 +0530

    Time: 10 ms (32.37%), Space: 7.9 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -6,18 +6,13 @@ Two City Scheduling
 **/
 class Solution {
 public:
-    int twoCityCostOptimum(vector<vector<int>> &costs, int a, int b, int n,unordered_map<string,int>&dp){
-        if(a == 0 && b == 0) return 0;
-        string s = to_string(a) + "_" + to_string(b);
-        if(dp.find(s) != dp.end()) return dp[s];
-        if(a == 0) dp[s] = costs[n][1] + twoCityCostOptimum(costs, a, b-1, n-1, dp);
-        else if(b == 0) dp[s] = costs[n][0] + twoCityCostOptimum(costs, a-1, b, n-1, dp);
-        else dp[s] = min(costs[n][0]+twoCityCostOptimum(costs, a-1, b, n-1, dp), costs[n][1]+twoCityCostOptimum(costs, a, b-1, n-1, dp));
-        return dp[s];
+    bool static comp(vector<int> &a, vector<int> &b){
+        return a[0]-a[1] < b[0]-b[1];
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size();
-        unordered_map<string,int>dp;
-        return twoCityCostOptimum(costs, n/2, n/2, n-1, dp);
+        sort(costs.begin(),costs.end(),comp);
+        int n = costs.size()/2, res = 0;
+        for(int i =0; i < n;i++) res += costs[i][0]+costs[i+n][1]; 
+        return res;
     }
 };
