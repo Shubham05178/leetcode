94fad1548019cd327d1be960f720fb82fdee6f94
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-12 00:50:17 +0530

    Time: 28 ms (84.50%), Space: 7.7 MB (79.75%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,7 +1,7 @@
 /**
 * Leetcode Problem - https://leetcode.com/problems/wildcard-matching/
 * Author - Shubham Nagaria
-* Date - 22nd March 2022
+* Date - 12th April 2022
 Wildcard Matching
 **/
 class Solution {
@@ -11,21 +11,27 @@ public:
         int m = p.size();
         bool dp[n+1][m+1];
         dp[0][0] = true;
-        for(int i = 0 ; i <= n; ++i){
-            for(int j = 0; j <= m; ++j){
+        for(int i =0; i <= n;++i){
+            for(int j =0 ; j<=m ; ++j){
                 if(i == 0 && j == 0) continue;
-                if(i == 0){
+                else if(i == 0){
                     if(p[j-1]=='*') dp[i][j] = dp[i][j-1];
-                    else dp[i][j] = false;
+                    else
+                        dp[i][j] = false;
                     continue;
                 }
-                if(j == 0){
+               else if(j == 0){
                     dp[i][j] = false;
                     continue;
+            }
+            else{
+                if(s[i-1] == p[j-1] || p[j-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if ( p[j-1]=='*')
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else
+                    dp[i][j] = false;
                 }
-                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
-                else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                else dp[i][j] = false;
             }
         }
         return dp[n][m];
