02118a76d9abf6789692ba77ed71c75238059c53
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-12 00:31:19 +0530

    Time: 10 ms (80.73%), Space: 7.3 MB (72.83%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,26 +1,28 @@
 /**
 * Leetcode Problem - https://leetcode.com/problems/edit-distance/
 * Author - Shubham Nagaria
-* Date - 22nd March 2022
+* Date - 12th April 2022
 Edit Distance
 **/
 class Solution {
 public:
-    int minDistanceSolver(string &word1, string &word2, int i, int j,vector<vector<int>> &dp){
-        if(i == -1 && j == -1) return 0;
-        else if(i == -1 && j!= -1) return j+1;
-        else if(j ==-1 && i != -1) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j])
-            dp[i][j] = minDistanceSolver(word1, word2, i-1, j-1,dp);
-        else
-            dp[i][j] = 1 + min(minDistanceSolver(word1, word2, i-1, j,dp), min(minDistanceSolver(word1, word2, i-1, j-1,dp), minDistanceSolver(word1, word2, i, j-1,dp)));
-        return dp[i][j];
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n, vector<int>(m,-1));
-        return  minDistanceSolver(word1, word2, n-1, m-1,dp);
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        for(int i = 0; i <= n ; ++i){
+            for(int j =0; j <= m;++j){
+                if(i ==0) dp[i][j] = j;
+                else if(j==0) dp[i][j] = i;
+                else{
+                    if(word1[i-1] == word2[j-1])
+                        dp[i][j]= dp[i-1][j-1];
+                    else
+                        dp[i][j]= 1+ min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                  }
+            }
+        }
+        return dp[n][m];
     }
 };
