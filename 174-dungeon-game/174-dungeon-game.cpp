c2dfeb1777e4fd7b88518a4c1ba0293a3c809c55
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-11 23:38:05 +0530

    Time: 12 ms (32.04%), Space: 8.8 MB (68.99%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,7 +1,7 @@
 /**
 * Leetcode Problem - https://leetcode.com/problems/dungeon-game/
 * Author - Shubham Nagaria
-* Date - 27th March 2022
+* Date - 11th April 2022
 Dungeon Game
 **/
 class Solution {
@@ -9,15 +9,15 @@ public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int n = dungeon.size();
         int m = dungeon[0].size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1,1));
-        for(int i = n - 1; i>=0; i--){
-            for(int j = m - 1; j>=0; j--){
-                    if( i == n-1 && j != m-1) 
-                    dp[i][j] = max(1, dp[i][j+1]-dungeon[i][j]);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,1));
+        for(int i = n-1; i >= 0; --i){
+            for(int j = m-1; j >= 0; --j){
+                if(i == n-1 && j != m-1)
+                    dp[i][j] = max(1, dp[i][j+1] - dungeon[i][j]);
                 else if( i != n-1 && j == m-1)
-                   dp[i][j]= max(1, dp[i+1][j] - dungeon[i][j]);
+                    dp[i][j] = max(1, dp[i+1][j] - dungeon[i][j]);
                 else
-                    dp[i][j]= max(1, min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]);
+                    dp[i][j] = max(1, min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j]); 
             }
         }
         return dp[0][0];
