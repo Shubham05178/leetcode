665e0a36cecac50ee262cb7f6b433711ef14a4f0
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-23 10:36:46 +0530

    Time: 10 ms (17.07%), Space: 7.1 MB (45.21%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,34 @@
+/**
+* Leetcode Problem - https://leetcode.com/problems/encode-and-decode-tinyurl/
+* Author - Shubham Nagaria
+* Date - 23rd April 2022
+Encode and Decode TinyURL
+**/
+class Solution {
+public:
+    unordered_map<string,string>encodeurls;
+    string getRandom(int length){
+        string alphanumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        string resultstring;
+        for(int index = 0; index < length ; index++)
+            resultstring += alphanumeric[rand()%alphanumeric.size()];
+        return resultstring;
+    }
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        string tiny = "http://tinyurl.com/"+getRandom(6);
+        if(encodeurls.find(tiny)!=encodeurls.end())
+            return encode(longUrl);
+        encodeurls[tiny]= longUrl;
+        return tiny;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return encodeurls[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
