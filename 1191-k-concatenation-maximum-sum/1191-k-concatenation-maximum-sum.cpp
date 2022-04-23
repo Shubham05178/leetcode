c56ff7685ae75ad5176af5cf005acc19d2b061af
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-23 11:17:03 +0530

    Time: 85 ms (45.94%), Space: 39.6 MB (69.19%) - LeetHub

---
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -0,0 +1,26 @@
+/**
+* Leetcode Problem - https://leetcode.com/problems/k-concatenation-maximum-sum/
+* Author - Shubham Nagaria
+* Date - 23rd April 2022
+K-Concatenation Maximum Sum
+**/
+class Solution {
+public:
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        int mod = 1e9+7;
+        long long int maxSum=0, currSum=0;
+        int m = min(k, 2);
+        long int totalsum = 0;
+        while(m--){
+            totalsum = 0;
+            for(int i: arr){
+                currSum += i;
+                totalsum += i;
+                maxSum = max(maxSum,currSum);
+                if(currSum < 0)
+                    currSum = 0;
+            }
+        }
+        return max(maxSum, k>2 ? maxSum+totalsum * (k-2):0)%mod;
+    }
+};
