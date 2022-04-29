a85eb4224631d49321f369142f60a3ec50b30de5
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-29 09:14:40 +0530

    Time: 36 ms (39.48%), Space: 13.5 MB (57.42%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,29 +1,27 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/is-graph-bipartite/
-* Author - Shubham Nagaria
-* Date - 4th April 2022
-Is Graph Bipartite?
-**/
 class Solution {
 public:
-    bool dfs(int src, vector<vector<int>> &graph, vector<int> &group){
-        for(auto j : graph[src]){
-            if(group[j]==-1){
-                group[j] = 1 - group[src];
-                if(!dfs(j,graph,group)) return false;
-            }
-            else if(group[j]== group[src])
-                return false;
-        }
-        return true;
+    bool dfs(int srcNode, vector<int>& groupAllocated, vector<vector<int>>& graph) {
+	    for(int &neighbourNode	: graph[srcNode]) {
+		    if(groupAllocated[neighbourNode] == -1) {
+		    	groupAllocated[neighbourNode] = 1 -  groupAllocated[srcNode];
+		    	if(!dfs(neighbourNode, groupAllocated, graph))
+			    	return false;
+	    	}
+	    	else if(groupAllocated[srcNode] == groupAllocated[neighbourNode]) {
+			    return false;
+		    }
+	    }
+    	return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int>group(n,-1);
-        for(int i = 0; i < n; i++){
-            if(group[i] == -1){
-                group[i] = 1;
-                if(!dfs(i,graph,group)) return false;
+        int totalNodes = graph.size();
+        vector<int> groupAllocated(totalNodes, -1); // -1 denotes no group allocated
+        for(int node = 0; node < totalNodes; node++) {
+            if(groupAllocated[node] == -1) {
+                groupAllocated[node] = 1; // allocating 1st group
+                if(!dfs(node, groupAllocated, graph)) {
+                    return false;
+                }
             }
         }
         return true;
