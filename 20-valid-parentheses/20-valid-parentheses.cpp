6d2c96679bf62cbee3cf86dc88c4107da0b3c481
Shubham Nagaria <subhamnagaria@gmail.com>
2022-04-30 21:39:28 +0530

    Time: 9 ms (5.28%), Space: 6.3 MB (51.61%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,33 +1,20 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/valid-parentheses/
-* Author - Shubham Nagaria
-* Date - 13th March 2022
-Valid Parentheses
-**/
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(char i : s){
-            if(i=='}'){
-                if(st.empty()  || st.top()!='{')
-                    return false;
-                st.pop();
-            }
-            else  if(i==']'){
-                if(st.empty()  || st.top()!='[')
-                    return false;
-                st.pop();
-            }
-            else if(i==')'){
-                if(st.empty()  || st.top()!='(')
-                    return false;
-                st.pop();
-            }
-            else{
-                st.push(i);
-            }
+        stack<char> st;
+        for(char &ch : s) {
+          if(ch == '{' || ch == '[' || ch == '(')
+            st.push(ch);
+          else{
+            if(ch == '}' && ( st.size() == 0 || st.top() != '{'))
+              return false;
+            if(ch == ']' && (st.size() == 0 || st.top() != '['))
+              return false;
+            if(ch == ')' && (st.size() == 0 || st.top() != '('))
+              return false;
+            st.pop();
+          }
         }
-        return st.empty();
+      return st.size() == 0;
     }
 };
