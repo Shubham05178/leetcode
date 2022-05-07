6129f3175187d7842fb093a38db90e59211701e4
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-07 18:16:05 +0530

    Time: 89 ms (89.27%), Space: 35.4 MB (6.94%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,12 +4,40 @@ public:
         int arrsize = nums.size();
         if(arrsize < 5) 
           return 0;
-        partial_sort(nums.begin(), nums.begin() + 4, nums.end());
-        nth_element(nums.begin() + 4, nums.end() - 4, nums.end());
-        sort(nums.end() - 4, nums.end());
-        int minDiff = INT_MAX;
-        for(int times = 0; times < 4; times++)
-          minDiff = min(minDiff, nums[arrsize - 4 + times] - nums[times]);
-      return minDiff;
+        multiset<int>maxElem, minElem;
+
+        for(int idx = 0; idx < arrsize; idx++) {
+          if(maxElem.size() < 4){
+            maxElem.insert(nums[idx]);
+          }
+          else if( nums[idx] > *maxElem.begin()){
+            maxElem.erase(maxElem.begin());
+            maxElem.insert(nums[idx]);
+          }
+          if(minElem.size() < 4) {
+            minElem.insert(nums[idx]);
+          }
+          else if( nums[idx] < *minElem.rbegin()){
+            minElem.erase(minElem.find(*minElem.rbegin()));
+            minElem.insert(nums[idx]);
+          }
+        }
+      int mini = INT_MAX;
+      auto iter1 = maxElem.begin(), iter2 = minElem.begin();
+      while(iter1 != maxElem.end() && iter2 != minElem.end()){
+        mini = min( mini, (*iter1) - (*iter2));
+        iter1++;
+        iter2++;
+      }
+      return mini;
     }
-};
+};  
+
+//[20,66,68,57,45,18,42,34,37,58]
+//[18,20,34,37,42,45,57,58,66,68]
+// [1,5,0,10,14]
+// [0,1,5,10,14] 
+// [0,1,5]   [ 5, 10, 14]
+//[6,6,0,1,1,4,6]
+// [0,1,1,4,6,6,6]
+    // [0,1,1,4] [4,6,6,6]
