32b0b7d0d4518370800786f7641f4f0d3ab49b42
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-10 21:36:22 +0530

    Time: 43 ms (65.24%), Space: 14.1 MB (78.07%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,24 +1,16 @@
-class Solution {
-public:
-    vector<string> generateParenthesis(int n) {
-        stack<pair<string,pair<int,int>>>generator;
-        vector<string>ans;
-        generator.push({"(",{1,0}});
-        while(generator.size()) {
-            auto toppair = generator.top();
-            generator.pop();
-            int l = toppair.second.first;
-            int r = toppair.second.second;
-            string topstr = toppair.first;
-            if(l - r < 0 || l > n || r > n )
-                continue;
-            if(l == n && r == n){
-                ans.push_back(topstr);
-                continue;
-            }
-            generator.push({topstr+")",{l,r+1}});
-            generator.push({topstr+"(", {l+1,r}});
-        }
-        return ans;
-    }
-};
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans =[]
+        generator = [["(",1,0]]
+        while generator:
+            s , l , r = generator.pop()
+            if l-r <0 or l>n or r>n:
+                continue
+            if l == n and r == n:
+                ans.append(s)
+                continue
+            generator.append([s+"(",l+1,r])
+            generator.append([s+")",l,r+1])
+        return ans
+            
+        
