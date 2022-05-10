758281359181492981c84c2e029a0c603aa8ec3e
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-10 20:26:10 +0530

    Time: 58 ms (41.42%), Space: 89.3 MB (87.62%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -4,27 +4,25 @@ public:
         int size1 = nums1.size();
         int size2 = nums2.size();
         if(size1 > size2)
-            return findMedianSortedArrays(nums2, nums1);
-        int low = 0 , high = size1, midX, midY;
-        double maxleftX, minRightX, maxleftY, minRightY;
+          return  findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = size1, mid1, mid2, leftmax1,leftmax2, rightmin1, rightmin2;
         while(low <= high) {
-            midX = low + ((high - low) >> 1);
-            midY = ((size1 + size2 + 1) >> 1) - midX;
-            maxleftX = (midX == 0) ? INT_MIN : nums1[midX - 1];
-            minRightX = (midX == size1) ? INT_MAX : nums1[midX];
-            maxleftY = (midY == 0) ? INT_MIN : nums2[midY - 1];
-            minRightY = (midY == size2) ? INT_MAX : nums2[midY];
-            if( maxleftY <= minRightX && maxleftX<= minRightY){
-                if((size1 + size2) % 2 == 0)
-                    return (max(maxleftY, maxleftX) + min(minRightX, minRightY)) / 2;
-                else
-                    return max(maxleftY, maxleftX);
+            mid1 = low + ((high - low) >> 1);
+            mid2 = ((size1 + size2 + 1) >> 1) - mid1;
+            leftmax1 = (mid1 == 0 ) ? INT_MIN : nums1[mid1 - 1];
+            leftmax2 = (mid2 == 0 ) ? INT_MIN : nums2[mid2 - 1];
+            rightmin1 = (mid1 == size1) ? INT_MAX : nums1[mid1];
+            rightmin2 = (mid2 == size2) ? INT_MAX : nums2[mid2];
+            if(leftmax1 <= rightmin2 && leftmax2 <= rightmin1){
+                if((size1 + size2) % 2 == 0) 
+                    return (max(leftmax1, leftmax2) + min(rightmin1, rightmin2))/2.0;
+                return max(leftmax1, leftmax2);
             }
-            else if(maxleftX > minRightY)
-                    high = midX - 1;
-            else
-                    low = midX + 1;
+            else if( leftmax1 > rightmin2)
+                high = mid1 -1;
+            else 
+                low = mid1 + 1;
         }
-        return -1.0;
+        return 0.0;
     }
 };
