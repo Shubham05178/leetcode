bb30183186013a82328eb686bddc2a67abca2a3d
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-10 09:40:50 +0530

    Time: 4 ms (34.38%), Space: 6.5 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,29 +1,24 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/combination-sum-iii/
-* Author - Shubham Nagaria
-* Date - 11th March 2022
-Combination Sum III
-**/
 class Solution {
 public:
-    void createCombinations(vector<vector<int>> &res, vector<int> &temp, int i,int sum,int k,int n){
-        if(temp.size()==k){
-            if(sum == n) res.push_back(temp);
+    vector<vector<int>>res;
+    void Solver(int idx, vector<int>&combination, int sum, int k, int n) {
+        if(k == 0 && sum == n) {
+            res.push_back(combination);
             return;
         }
-        for(int j = i; j < 10; j++){
-            sum += j;
-            temp.push_back(j);
-            createCombinations(res,temp,j+1,sum,k,n);
-            sum -= j;
-            temp.pop_back();
+        if( k == 0 || sum > n)
+            return;
+        for(int idy = idx; idy <= 9; idy++ ){
+            sum += idy;
+            combination.push_back(idy);
+            Solver(idy + 1, combination, sum, k - 1, n);
+            combination.pop_back();
+            sum -= idy;
         }
-        return;
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>temp;
-        vector<vector<int>>res;
-        createCombinations(res,temp,1,0,k,n);
+        vector<int>combination;
+        Solver(1, combination, 0, k, n);
         return res;
     }
 };
