0f49332d105cd1d12ce1b9f2dcdadd9605db3f03
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-11 09:18:35 +0530

    Time: 505 ms (30.73%), Space: 121.2 MB (29.08%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -3,15 +3,17 @@ public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int size1 = nums1.size(), size2 = nums2.size();
         vector<vector<int>>ans;
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int> > > minsum;
-        for(int idx = 0; idx < size1; idx++) 
-            minsum.push({nums1[idx] + nums2[0],idx,0});
-        while(k-- && minsum.size()) {
+        priority_queue< vector<int>> minsum;
+        for(int idx = 0; idx < size1 && idx < k; idx++) 
+            minsum.push({-(nums1[idx] + nums2[0]),idx,0});
+        while(k-- && minsum.size() > 0) {
             auto top = minsum.top();
+            int idx = top[1];
+            int idy = top[2];
             minsum.pop();
-            ans.push_back({nums1[top[1]], nums2[top[2]]});
-            if(top[2] + 1 < size2)
-                minsum.push({nums1[top[1]] + nums2[top[2] + 1], top[1], top[2] + 1});
+            ans.push_back({nums1[idx], nums2[idy]});
+            if(idy + 1 < size2)
+                minsum.push({-(nums1[idx] + nums2[idy + 1]), idx, idy + 1});
         }
         return ans;
     }
