58c36a5ebe396ad868f40bb3252c78ab163db480
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-11 13:17:05 +0530

    Time: 53 ms (70.11%), Space: 7.6 MB (81.54%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    bool solver(int idx1, int  idx2, string &s, string &p, vector<vector<int>>& dp){
-        if(idx1 == -1 && idx2 == -1)
-            return true;
-        if(idx1 == -1){
-            while(idx2 >=0){
-                if(p[idx2] != '*')
-                    return false;
-                idx2--;
-            }
-            return true;
-        }
-        if(idx2 == -1)
-            return false;
-        if(dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
-        if(p[idx2] == '?' || p[idx2] == s[idx1]) {
-            dp[idx1][idx2] = solver(idx1 -1, idx2 - 1 ,s, p, dp);
-        }
-        else if(p[idx2]=='*'){
-            dp[idx1][idx2] = solver(idx1 - 1, idx2, s, p, dp) || solver(idx1, idx2 - 1, s, p, dp);
-        }
-        else
-            dp[idx1][idx2] = false;
-        return dp[idx1][idx2];
-        
-    }
     bool isMatch(string s, string p) {
         int size1 = s.size();
         int size2 = p.size();
-        vector<vector<int>>dp(size1, vector<int>(size2, -1));
-        return solver(size1 - 1, size2 - 1, s, p, dp);
+        bool dp[size1 + 1][size2 + 1];
+        dp[0][0] = true;
+        for(int idx = 0 ; idx <= size1 ; idx++) {
+            for(int idy = 0; idy <= size2; idy++) {
+                if( idx == 0 && idy == 0) continue;
+                if(idx == 0){
+                    if(p[idy-1] == '*') dp[idx][idy] = dp[idx][idy - 1];
+                    else dp[idx][idy] = false;
+                    continue;
+                }
+                if(idy == 0){
+                    dp[idx][idy] = false;
+                    continue;
+                }
+                if(( p[idy - 1] == s[idx - 1] ) || p[idy -1]=='?')
+                    dp[idx][idy] = dp[idx - 1][idy - 1];
+                else if(p[idy - 1] == '*')
+                    dp[idx][idy] = dp[idx - 1][idy] || dp[idx][idy - 1];
+                else
+                    dp[idx][idy]= false;
+            }
+        }
+        return dp[size1][size2];
     }
 };
