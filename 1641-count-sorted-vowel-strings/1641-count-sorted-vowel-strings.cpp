6f95ba3872502d026e701edf5b828b6a7459ae77
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-11 09:44:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,23 +1,15 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/count-sorted-vowel-strings/
-* Author - Shubham Nagaria
-* Date - 24th March 2022
-Count Sorted Vowel Strings
-**/
 class Solution {
 public:
-    int Ncr(int n , int r){
-        int m= min(r,n-r);
-        long long mul1 =1 ,mul2=1;
-        while(m>0){
-            mul1 *= m;
-            mul2 *= n;
-            n--;
-            m--;
-        }
-        return mul2/mul1;
-    }
     int countVowelStrings(int n) {
-        return Ncr(5+n-1,n);
+        vector<int>pattern(5,1);
+        int sumi = 0;
+        for(int idx = 1; idx <= n;idx++){
+            sumi = 1;
+            for(int idy = 1; idy < 5; idy++){
+                sumi+= pattern[idy];
+                pattern[idy] = sumi;
+            }
+        }
+        return sumi;
     }
 };
