f7d95015d5cc68c5b56cb5e3a32607505082d7a9
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-12 16:15:57 +0530

    Time: 16 ms (44.91%), Space: 8.2 MB (99.81%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,23 +1,11 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/permutations-ii/
-* Author - Shubham Nagaria
-* Date - 13th March 2022
-Permutations II
-**/
 class Solution {
 public:
-    void Permutations(int i, int n, set<vector<int>> &s, vector<int>& nums){
-        if(i == n) s.insert(nums);
-        for(int j = i; j < n; j++){
-            swap(nums[i],nums[j]);
-            Permutations(i+1, n , s, nums);
-            swap(nums[i],nums[j]);
-        }
-    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>>s;
-        Permutations(0, nums.size(), s, nums);
-        vector<vector<int>>res(s.begin(),s.end());
-        return res;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        do{
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        return ans;
     }
 };
