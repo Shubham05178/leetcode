b90848b0c05f2862922a0b3d5affada55e6aad73
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-12 16:41:26 +0530

    Time: 8 ms (36.93%), Space: 6.6 MB (43.05%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         int arr[10];
-        int countbulls = 0,countcows =0;
+        int cA = 0,cB = 0;
         int size = secret.size();
         memset(arr,0,sizeof(arr));
         for(int idx = 0; idx < size; idx++ ){
             if(secret[idx] == guess[idx]){
-                countbulls++;
+                cA++;
                 continue;
             }else{
-                if(arr[secret[idx]-'0'] < 0){
-                    countcows++;
-                }
+                if(arr[secret[idx]-'0'] < 0)
+                    cB++;
                 arr[secret[idx]-'0']++;
                 arr[guess[idx]- '0']--;
-                if(arr[guess[idx]-'0'] >= 0){
-                    countcows++;
-                }
+                if(arr[guess[idx]-'0'] >= 0)
+                    cB++;
             }
         }
-        return to_string(countbulls)+"A"+to_string(countcows)+"B";
+        return to_string(cA)+"A"+to_string(cB)+"B";
     }
 };
