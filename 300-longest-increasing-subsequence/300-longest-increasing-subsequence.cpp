177cc8cb1ed1d9ccc1c8ecba5f52d4639303d1fd
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-12 15:18:58 +0530

    Time: 12 ms (83.19%), Space: 10.5 MB (66.92%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,40 +1,13 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/longest-increasing-subsequence/
-* Author - Shubham Nagaria
-* Date - 10th April 2022
-Longest Increasing Subsequence
-**/
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size(), low, high, mid, ans, l = 0;
-        vector<int>dp(n,INT_MIN);
-        for(int i = 0;i < n;i++){
-            if(i ==0){
-                dp[l]= nums[i];
-                l++;
-            }
-            else{
-                if(dp[l-1] < nums[i]){
-                    dp[l]= nums[i];
-                    l++;
-                }
-                else{
-                    low = 0, high = l-1, mid,ans = 0;
-                    while(low<=high){
-                        mid = low + ((high -low)>>1);
-                        if(nums[i] <= dp[mid]){
-                            ans = mid;
-                            high = mid -1;
-                        }
-                        else{
-                            low = mid+1;
-                        }
-                    }
-                    dp[ans]=nums[i];
-                }
-            }
+        vector<int>lis;
+        for( int i : nums){
+        int idx =  lower_bound(lis.begin(),lis.end(),i) - lis.begin();
+            if(idx==lis.size())
+                lis.push_back(i);
+            lis[idx]=i;
         }
-        return l;
+        return lis.size();
     }
 };
