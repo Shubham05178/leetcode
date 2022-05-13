f27b5e5afb39de32a25cd2091b1681452c8d8079
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-14 00:34:10 +0530

    Time: 1322 ms (5.05%), Space: 39.2 MB (83.77%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.cpp
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-       long int sumofalice =0, sumofbob =0;
-        for(int i : aliceSizes )
-            sumofalice +=i;
-        for(int i : bobSizes )
-            sumofbob += i;
-        for(int i : aliceSizes){
-            for(int j : bobSizes){
-                if((sumofalice+j)-i == (sumofbob+i)-j)
-                    return {i,j};
+        long int asum = 0, bsum = 0;
+        for(int idx: aliceSizes)
+            asum+= idx;
+         for(int idx : bobSizes)
+            bsum+= idx;
+        for(int idx :aliceSizes){
+            for(int idy: bobSizes){
+                if((asum-idx+idy) ==( bsum -idy +idx))
+                return {idx,idy};
             }
         }
         return {};
     }
 };
+
+/**
+aliceSizes = [1,1], bobSizes = [2,2]
+
+
+alicesum =2 bobsum = 4
+
+alicsum =2 -1 +2 = 3
+bobsum = 4+1-2=3
+
+
+
+
+
+**/
+// 
