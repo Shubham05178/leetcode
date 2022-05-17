f9db09b8dd66201fc1d96f378be9780e7dfe0e53
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-18 00:42:56 +0530

    Time: 55 ms (84.16%), Space: 14 MB (35.80%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.py
+++ 929-unique-email-addresses/929-unique-email-addresses.py
@@ -3,8 +3,8 @@ class Solution:
         emailsSet = set()
         for email in emails:
             localname, domain = email.split("@")
-            remainlocalname=localname.split("+")
+            remainlocalname = localname.split("+")
             name = remainlocalname[0].replace(".","")   
-            name+= "@" + domain
+            name += "@" + domain
             emailsSet.add(name)
         return len(emailsSet)
