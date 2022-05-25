034c09b4a888fd6ef560a14ddcb1197e998b049c
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-25 21:05:53 +0530

    Time: 2239 ms (16.95%), Space: 61.8 MB (46.10%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -1,14 +1,11 @@
-import bisect
 class Solution:
     def maxEnvelopes(self, envelopes):
         envelopes.sort(key = lambda x: (x[0], -x[1]))
+        print(envelopes)
         lish = []
-        
         for w, h in envelopes:
             idx = bisect.bisect_left(lish, h)
-            
             if idx == len(lish):
                 lish.append(h)
             lish[idx] = h
-            
         return len(lish)
