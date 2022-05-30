3ad17d3711578a2ae695d20871bd8cca10d53afd
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-30 23:28:10 +0530

    Time: 11 ms (91.18%), Space: 8.4 MB (98.12%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string B, string A) {
-    unordered_map < char , int > mp;
-    int need = A.size();
-    int m = need;
-    int n = B.size();
-    vector<int> res;
-    for( char c : A )
-        mp[c]++;
-    for(int i =0; i < n ; i++ ){
-        if(i- m >= 0 && ++ mp[B[i - m]] > 0) need++;
-        mp[B[i]]--;
-        if(mp[B[i]] > -1 ) need--; 
-        if(need == 0 )
-            res.push_back(i - m + 1);
-    }
-    return res;
+    vector<int> findAnagrams(string s, string p) {
+        int need = p.size(), nsize = s.size();
+        int msize = need;
+        vector<int>res,words(26);
+        for(char ch : p)
+            words[ch-'a']++;
+        for(int idx = 0 ; idx < nsize; idx++) {
+            if(idx - msize >= 0 && ++words[s[idx - msize]-'a']> 0 )
+                need++;
+            words[s[idx]-'a']--;
+            if(words[s[idx]-'a']>-1)
+                need--;
+            if(need == 0)
+                res.push_back(idx - msize + 1);
+        }
+        return res;
     }
 };
