120fa548ea31b77fde5994781645a2ea7ade8fd6
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-31 14:30:39 +0530

    Time: 128 ms (61.89%), Space: 8.1 MB (67.72%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,43 +1,33 @@
-/**
-* Leetcode Problem - https://leetcode.com/problems/palindrome-partitioning-ii/
-* Author - Shubham Nagaria
-* Date - 4th April 2022
-Palindrome Partitioning II
-**/
 class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        vector<int>dp(n, INT_MAX);
-        dp[0]=0;
-        int palindrome[n][n];
-        memset(palindrome,-1,sizeof(palindrome));
-        int j =0, i_, j_;
-        while(j<n){
-           i_=0, j_=j;
-            while(i_ < n && j_ < n){
-                if(i_ == j_)
-                    palindrome[i_][j_] = 1;
-                else if(s[i_]== s[j_]){
-                    palindrome[i_][j_] = 1;
-                    if( j_ - i_ >1)
-                         palindrome[i_][j_] = palindrome[i_ + 1][j_ - 1];
+        vector<vector<bool>>p(n, vector<bool>(n, false));
+        vector<int>dp(n,INT_MAX);
+        for(int diff = 0; diff < n; ++diff) {
+            for(int i = 0, j = diff; j < n && i < n ; i++, j++){
+                if(i== j){
+                    p[i][j]=true;
                 }
-                else 
-                     palindrome[i_][j_] = 0;
-                i_++;
-                j_++;
+                else if(j - i == 1){
+                    if(s[i]==s[j])
+                        p[i][j]=true;
+                }
+                else{
+                    if(s[i]==s[j])
+                        p[i][j]= true;
+                    p[i][j]= p[i][j]&& p[i+1][j-1];
+                }
             }
-            j++;
         }
-        for(int i =1 ; i < n ;i++){
-            if(palindrome[0][i] == 1) dp[i]=0;
+      dp[0]=0;
+        for(int j = 0; j < n; j++){
+            if(p[0][j]) dp[j] =0;
             else{
-                for(int j =1; j <=i ; j++){
-                    if(palindrome[j][i] == 1){
-                        dp[i] = min(dp[i], 1+ dp[j-1]);
-                    }
-                }   
+                for(int i = 1; i <= j;i++){
+                    if(p[i][j])
+                        dp[j]= min(dp[j], 1+ dp[i-1]);
+                }
             }
         }
         return dp[n-1];
