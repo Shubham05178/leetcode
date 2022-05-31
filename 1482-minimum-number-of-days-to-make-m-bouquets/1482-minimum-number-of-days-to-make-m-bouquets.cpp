dd480c7a5424838d1dfb5bc8da0a7645feb331cf
Shubham Nagaria <subhamnagaria@gmail.com>
2022-05-31 09:50:53 +0530

    Time: 222 ms (53.40%), Space: 63.4 MB (25.63%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     bool check(vector<int>& bloomDay,int days, int m, int k){
-        int count = 0, lc =0;
+        int countbotique = 0, dayscontinuous = 0;
         for(int elem :bloomDay ){
             if(elem <= days)
-                lc++;
+                dayscontinuous++;
             else
-                lc = 0;
-            if(lc == k){
-                count++;
-                lc = 0;
+                dayscontinuous = 0;
+            if(dayscontinuous == k){
+                countbotique++;
+                dayscontinuous = 0;
             }
         }
-        return count >= m;
+        return countbotique >= m;
     } 
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
-        if(n< m *k) return -1;
+        if(n < m * k) 
+            return -1;
         int high = *max_element(bloomDay.begin(), bloomDay.end());
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int low  = *min_element(bloomDay.begin(), bloomDay.end());
         int ans = high, mid;
         while(low <= high){
-            mid = low + ((high - low)>>1);
-            if(check(bloomDay,mid,m,k)){
+            mid = low + ((high - low) >> 1);
+            if(check(bloomDay, mid, m, k)){
                 ans = mid;
-                high = mid -1;
+                high = mid - 1;
             }
             else{
                 low = mid + 1;
