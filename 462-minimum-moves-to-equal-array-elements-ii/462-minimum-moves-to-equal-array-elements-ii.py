fdcdfbf1269f657c7b27f944617eec8bd5512b7e
Shubham Nagaria <subhamnagaria@gmail.com>
2022-07-01 00:45:37 +0530

    Time: 98 ms (67.99%), Space: 15.4 MB (33.43%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        n = len(nums)
-        m = 0
-        dp = [0 for _ in range(n)]
-        for i in range(1, len(nums)):
-            dp[i] = (nums[i] - nums[i-1])* i + dp[i-1]
-        ans = dp[n-1]
-        for i in range(len(nums)-2,-1,-1):
-            m += (nums[i + 1] - nums[ i ]) * (n - 1 - i)
-            ans = min(ans, m + dp[i])
+        size = len(nums)
+        noofmovesfromrighttoleft = 0
+        dp = [0 for _ in range(size)]
+        for idx in range(1, size):
+            dp[idx] = (nums[idx] - nums[idx - 1]) *  idx + dp[idx - 1]
+        ans = dp[size - 1]
+        for idx in range(size - 2, -1, -1):
+            noofmovesfromrighttoleft += (nums[idx + 1] - nums[idx]) * (size - 1 - idx)
+            ans = min(ans, noofmovesfromrighttoleft + dp[idx])
         return ans
         
