5dd9e8a08c00ee6c3f6ad1a394fc693673595875
Shubham Nagaria <subhamnagaria@gmail.com>
2022-07-01 19:29:03 +0530

    Time: 92 ms (25.69%), Space: 14.5 MB (19.68%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,17 +1,18 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = {}
-        def mincost(idx,maxDay):
-            if idx == len(days):
+        dayCost, weekCost, monthCost = costs
+        jumpCosts = [[1, dayCost], [7, weekCost], [30, monthCost]]
+        @cache
+        def solver(idx):
+            if idx >= len(days):
                 return 0
-            if dp.get((idx,maxDay),-1) != -1:
-                return dp[(idx,maxDay)]
-            if maxDay > days[idx]:
-                dp[(idx,maxDay)] = mincost(idx+1, maxDay)
-            else:
-                dp[(idx,maxDay)] = min(costs[0]+ mincost(idx+1, days[idx]+1), costs[1] + mincost(idx+1,days[idx]+7), costs[2] + mincost(idx+1, days[idx]+30))
-            return dp[(idx,maxDay)]
-        return mincost(0,0)
-            
+            currDay = days[idx]
+            cost = float("inf")
+            for dayJump, jumpCost in jumpCosts:
+                newIdx = bisect.bisect_left(days, currDay + dayJump)
+                cost = min(cost, jumpCost + solver(newIdx))
+            return cost
+        return solver(0)
                 
+        
         
